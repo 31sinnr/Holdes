748eb7a8b17b9a0bff1da4f92da736f674f95e31
31sinnr <graph.idylls_0t@icloud.com>
2025-05-11 04:53:52 +0300

    Updated requestor.py

--- src/requestor.py
+++ src/requestor.py
@@ -1,18 +1,12 @@
+import requests, json, os, time, uuid, tldextract, sys
 import undetected_chromedriver as uc
-import requests
-import json
-import os
-import time
 from urllib.parse import urlparse
-import tldextract
-import uuid
-import time
+import style as s
 
 class Requestor:
 
     @staticmethod
     def load_captcha_data(url):
-
         domain = Requestor.extract_domain(url)
         filename = f"{domain}.STRKR" 
         if os.path.exists(filename):
@@ -33,47 +27,35 @@ class Requestor:
         parsed_url = urlparse(url)
         extracted = tldextract.extract(parsed_url.netloc)
         base_domain = f"{extracted.domain}.{extracted.suffix}"
-
         if bWithProtocol:
             return f"{parsed_url.scheme}://{base_domain}"
         return base_domain
-
 
     @staticmethod
     def start_browser(url):
         domain = Requestor.extract_domain(url)
         ShortUrl = Requestor.extract_domain(url, True)
-
         options = uc.ChromeOptions()
-
-        # Запуск браузера
         driver = uc.Chrome(options=options, headless=False)
-
-        # Устанавливаем таймаут для загрузки страницы
         driver.get(ShortUrl)
         time.sleep(7)
 
-        # Попытка извлечь данные, независимо от того, произошел ли таймаут или ошибка
         try:
             user_agent = driver.execute_script("return navigator.userAgent;")
             cookies = {c["name"]: c["value"] for c in driver.get_cookies()}
 
             if not cookies:
-                print(f"❌ Не удалось собрать cookies для домена {domain}")
+                s.error(f"Не удалось собрать cookies для домена {domain}")
             else:
-                print(f"✅ Собраны cookies для домена {domain}")
+                s.success(f"Собраны cookies для домена {domain}")
         except Exception as e:
-            print(f"❌ Ошибка при извлечении данных: {e}")
-
-        # Переходим к следующей части логики, не закрывая браузер
-        # Если страница не загрузилась, это не останавливает выполнение других операций
+            s.error(f"Ошибка при извлечении данных: {e}")
 
         if cookies:
             Requestor.save_captcha_data(url, user_agent, cookies)
         else:
-            print("❌ Ошибка при сохранении cookies, повторите попытку.")
+            s.error("Ошибка при сохранении cookies, повторите попытку.")
 
-        # В конце закрываем браузер
         driver.quit()
 
     @staticmethod
@@ -86,12 +68,12 @@ class Requestor:
             captcha_data = Requestor.load_captcha_data(url)
 
         domain = Requestor.extract_domain(url)
-
         session = requests.Session()
-
         urlForHeaders = url
+
         if "coin-stats.com" in url:
             urlForHeaders = "coinstats.com"
+
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 OPR/118.0.0.0 (Edition Yx GX)",
             "Referer": urlForHeaders,
@@ -107,20 +89,19 @@ class Requestor:
             "x-app-appearance": "dark",
             "platform": "web",
             "if-none-match": '',
-
-            "Uuid": str(uuid.uuid4())  # Вот здесь UUID
+            "Uuid": str(uuid.uuid4())
         }
 
         if bDoBypass:
             captcha_data = Requestor.load_captcha_data(url)
 
             if not captcha_data:
-                print(f"⚠️ Cookies для домена {domain} не найдены. Запускаем браузер для их получения...")
+                s.warning(f"Cookies для домена {domain} не найдены. Запускаем браузер для их получения...")
                 Requestor.start_browser(url)
                 captcha_data = Requestor.load_captcha_data(url)
 
             if captcha_data is None:
-                print("❌ Ошибка: cookies не загружены. Повторите попытку.")
+                s.error("Ошибка: cookies не загружены. Повторите попытку.")
                 return None
 
             session.cookies.update(captcha_data["cookies"])
@@ -130,21 +111,20 @@ class Requestor:
             response = session.get(url, headers=headers)
 
             if response.status_code == 403:
+
                 if not bDoBypass: 
-                    print(f"⚠️ Доступ запрещен. Перезапускаем браузер но уже с куками {domain}...")
-
+                    s.warning(f"Доступ запрещен. Перезапускаем браузер но уже с куками {domain}...")
                     return Requestor.make_request(url, True)
+                
                 elif bDoBypass and not bResetCookie:
-                    print(f"⚠️ Доступ запрещен. Перезапускаем браузер для обновления cookies для {domain}...")
-
+                    s.warning(f"Доступ запрещен. Перезапускаем браузер для обновления cookies для {domain}...")
                     return Requestor.make_request(url, True, True) 
 
-
-                print(f"❌ Ошибка: Доступ по-прежнему запрещен для {domain}.")
+                s.error(f"Ошибка: Доступ по-прежнему запрещен для {domain}.")
                 return None
 
             return response.text
 
         except requests.exceptions.RequestException as e:
-            print(f"❌ Ошибка при запросе: {e}")
+            s.error(f"Ошибка при запросе: {e}")
             return None
